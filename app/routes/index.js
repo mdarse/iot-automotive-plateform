16772db2ea4e3b5be5ff3e99d44932787a71c47c
Plucile <lucileperan@gmail.com>
2013-05-29 10:47:52 +0200

    add fake vehicule datas

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,11 +12,14 @@ exports.index = function(req, res, next) {
   var counter = 0;
   var finish = function() {
     counter ++;
-      if ( counter > 1 ) {
+      if ( counter >= 5 ) {
       res.render('index', {
         title: 'Remoteo',
         batteryStatus: batteryStatus,
-        temperature: temperature
+        temperature: temperature,
+        windowsStatus: windowsStatus,
+        lockingStatus: lockingStatus,
+        heatingStatus: heatingStatus
       })
     }
   }
@@ -33,59 +36,22 @@ exports.index = function(req, res, next) {
     finish();
   }) 
 
+  vehicle.getWindowsStatus(function(err, status) {
+    if (err) return next(err);
+    windowsStatus = status;
+    finish();
+  }) 
 
+  vehicle.getLockingStatus(function(err, status) {
+    if (err) return next(err);
+    lockingStatus = status;
+    finish();
+  })
 
-  // vehicle.getBatteryStatus(function(err, status) {
-  //   if (err) return next(err);
-  //   batteryStatus = status;
-  //   finish();
-  // }) 
+  vehicle.getHeatingStatus(function(err, status) {
+    if (err) return next(err);
+    heatingStatus = status;
+    finish();
+  })
 
-  // var counter = 1;
-  // function finish() {
-  //   counter ++;
-  //   if ( counter > 2 ) {
-  //     res.render('index', {
-  //       title: 'Home',
-  //       batteryStatus: batteryStatus
-  //     })
-  //   }
-  // }
-
-
-  // var temperature;
-  // vehicle.getTemperature(function(err, t) {
-  //   if (err) return next(err);
-  //   temperature = t;
-  //   finish();
-  // });
-
-  // var bat;
-  // vehicle.getBatteryStatus(function(err, status) {
-  //   if (err) return next(err);
-  //   bat = status;
-  //   console.log('ok');
-  //   finish();
-  // });
-
-  // var counter = 1;
-  // var finish = function() {
-  //   console.log('hi');
-  //   counter++;
-  //   // if (counter < 2) return;
-  //   res.render('index', {
-  //     title: 'Home',
-  //     // temperature: temperature,
-  //     batteryStatus: bat
-  //   });
-  // };
-
-  // vehicle.getBatteryStatus(function(err, status) {
-  //   if (err) return next(err);
-/*
-    res.render('index', {
-      title: 'Remoteo',
-      batteryStatus: status
-    });
-  });*/
 };
