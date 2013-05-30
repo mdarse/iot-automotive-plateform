6981b8e90d0fea8991e5fa27f311c2d5d5c70c17
anaelleko <kopffanaelle@gmail.com>
2013-05-30 11:10:30 +0200

    style + img

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+/*document.getElementById('about').addEventListener('click', aPropos, false);
+function aPropos() {
+	alert('Pourquoi une telle application ?');
+}
+
+document.getElementById('pane-open').addEventListener('click', changeState, false);
+document.getElementById('pane-close').addEventListener('click', changeState, false);
+document.getElementById('pane-half').addEventListener('click', changeState, false);
+function changeState() {
+	alert('Selectionné !');
+	this.class = 'selection';
+}
+*/
