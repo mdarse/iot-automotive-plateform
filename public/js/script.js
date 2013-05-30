7c241ab1f272d58a76cc5a3621beaa0cdade3953
anaelleko <kopffanaelle@gmail.com>
2013-05-30 15:52:02 +0200

    style + js

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,20 @@
-/*document.getElementById('about').addEventListener('click', aPropos, false);
-function aPropos() {
-	alert('Pourquoi une telle application ?');
-}
+/*
+ * Au clic sur un bouton, celui-ci devient sélectionné.
+ * On doit donc changer l'état du nouveau bouton mais aussi du bouton actuel
+ * car un seul bouton peut-être sélectionné à la fois.
+ * Parfois, il n'y a pas 2 mais 3 boutons.
+ */
 
-document.getElementById('pane-open').addEventListener('click', changeState, false);
-document.getElementById('pane-close').addEventListener('click', changeState, false);
-document.getElementById('pane-half').addEventListener('click', changeState, false);
-function changeState() {
-	alert('Selectionné !');
-	this.class = 'selection';
-}
+ /* si le bouton est cliqué,	-> addeventlistener
+ alors il est sélectionné	= style
+ alors il est disabled		= 
+
 */
+
+document.getElementById('door-close').addEventListener('click', changeEtat, false);
+document.getElementById('door-open').addeventlistener('click', changeEtat, false);
+
+function changeEtat() {
+//	this.disabled = disabled;
+	this.className = this.className + ' selection';
+}
